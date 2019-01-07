b065cccd07b63976363c5fa73354090edb2fb54f
Matthew Corrigan <corrigan1247@students.d211.org>
2019-01-07 19:05:16 +0000

    Add spacers, workshop teachers, improve open/close category script -Rob

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function openWorkshop(e) {
     var children = e.parentElement.children;
     for (var index = 0; index < children.length; index++) {
-        children[index].className = children[index].className.replace("hidden", " ");
+        children[index].className = children[index].className.replace("hidden", "shown");
     }
-    children[1].innerHTML = "See Less";
-    children[1].setAttribute("onclick", "closeWorkshop(this);");
+    children[1].innerHTML = "See Less"; //change 'see more' button to 'see less'
+    children[1].setAttribute("onclick", "closeWorkshop(this);"); //rebind the button to close the workshop
 }
 
 function closeWorkshop(e) {
     var children = e.parentElement.children;
-    children[2].className += " hidden";
-    children[3].className += " hidden";
-    //children[4].className += " hidden"; //leave difficulty shown
-    children[1].innerHTML = "See More";
-    children[1].setAttribute("onclick", "openWorkshop(this);");
+    for (var index = 0; index < children.length; index++) {
+        children[index].className = children[index].className.replace("shown", "hidden");
+    }
+    children[1].innerHTML = "See More"; //change 'see less' button to 'see more'
+    children[1].setAttribute("onclick", "openWorkshop(this);"); //rebind the button to open the workshop
 }
