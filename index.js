e4b0a2fa4c3843d4378c0a5200c49f080c6b9b45
Rob B <budak7273@students.d211.org>
2019-01-19 03:01:47 +0000

    Add collapseable schedule spreadsheet and update workshop descriptions

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function openWorkshop(e) {
     for (var index = 0; index < children.length; index++) {
         children[index].className = children[index].className.replace("hidden", "shown");
     }
-    children[1].innerHTML = "See Less"; //change 'see more' button to 'see less'
+    if(children[1].innerHTML == "See More")
+    {
+        children[1].innerHTML = "See Less"; //change 'see more' button to 'see less'
+    }
     children[1].setAttribute("onclick", "closeWorkshop(this);"); //rebind the button to close the workshop
 }
 
@@ -12,6 +15,9 @@ function closeWorkshop(e) {
     for (var index = 0; index < children.length; index++) {
         children[index].className = children[index].className.replace("shown", "hidden");
     }
-    children[1].innerHTML = "See More"; //change 'see less' button to 'see more'
+    if(children[1].innerHTML == "See Less")
+    {
+        children[1].innerHTML = "See More"; //change 'see less' button to 'see more'
+    }
     children[1].setAttribute("onclick", "openWorkshop(this);"); //rebind the button to open the workshop
 }
